d1116ee1780b2f191f5c87bdb83e0d7f01232dcd
Abdul Rahman Zahiri <abdulrahmanzahiri6644@gmail.com>
2025-05-29 02:58:09 -0230

    Add Game model with name and price

---
+++ DEV_SDAT_QAP-1/src/main/java/com/gamestore/domain/Game.java
@@ -0,0 +1,19 @@
+package com.gamestore.domain;
+
+public class Game {
+    private final String name;
+    private final double price;
+
+    public Game(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
